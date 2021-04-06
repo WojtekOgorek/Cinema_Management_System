8caf5f74606f7578b9cf0f84b77bd338f813d29c
WojtekOgorek <ogorek.wojciech@gmail.com>
2021-04-06 18:54:18 +0200

    infrastracture with pom added plus spring config file and application properties

---
+++ infrastructure/src/main/java/ogorek/wojciech/infrastructure/AppSpringConfig.java
@@ -0,0 +1,35 @@
+package ogorek.wojciech.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import org.jdbi.v3.core.Jdbi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("ogorek.wojciech")
+@PropertySource("classpath:application.properties")
+@RequiredArgsConstructor
+public class AppSpringConfig {
+
+
+        @Value("${db.url}")
+        private String databaseUrl;
+
+        @Value("${db.username}")
+        private String databaseUsername;
+
+        @Value("${db.password}")
+        private String databasePassword;
+
+
+        @Bean
+        public Jdbi jdbi() {
+            return Jdbi.create(databaseUrl, databaseUsername, databasePassword);
+        }
+
+
+
+}
